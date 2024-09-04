d5fb8fcb8414dae6baa9a737587045419afe0cca
Angelbyte96 <roberto.angel96@live.com>
2024-09-03 22:02:04 -0600

    Se anade funcion para templates de proyectos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './template-cardProyect.js';
+import { newProyect } from './template-cardProyect.js';
+
+newProyect(0);
+newProyect(1);
--- src/proyects.js
+++ src/proyects.js
@@ -6,6 +6,13 @@ const proyects = {
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
 			img: 'https://img.freepik.com/vector-gratis/estilo-neon-proximamente-diseno-fondo-brillante_1017-25516.jpg?w=1380&t=st=1725337096~exp',
 			list: ['HTML', 'CSS', 'JavaScript']
+		},
+		{
+			title: 'Proyect 2',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
+			img: 'https://img.freepik.com/vector-gratis/estilo-neon-proximamente-diseno-fondo-brillante_1017-25516.jpg?w=1380&t=st=1725337096~exp',
+			list: ['NodeJs', 'TypeScript']
 		}
 	]
 };
