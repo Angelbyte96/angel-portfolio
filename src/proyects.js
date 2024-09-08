6a0b8513e189b355cafef76cec25f16f6951ca3f
Angelbyte96 <roberto.angel96@live.com>
2024-09-08 12:03:33 -0600

    Se cambia la imagen de proyectos a local

--- src/proyects.js
+++ src/proyects.js
@@ -5,7 +5,7 @@ const proyects = {
 			title: 'Proyect 1',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
-			img: 'https://img.freepik.com/vector-gratis/estilo-neon-proximamente-diseno-fondo-brillante_1017-25516.jpg?w=1380&t=st=1725337096~exp',
+			img: './src/assets/coming soon.webp',
 			list: ['HTML', 'CSS', 'JavaScript']
 		},
 		{
@@ -13,7 +13,7 @@ const proyects = {
 			title: 'Proyect 2',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
-			img: 'https://img.freepik.com/vector-gratis/estilo-neon-proximamente-diseno-fondo-brillante_1017-25516.jpg?w=1380&t=st=1725337096~exp',
+			img: './src/assets/coming soon.webp',
 			list: ['NodeJs', 'TypeScript']
 		}
 	]
