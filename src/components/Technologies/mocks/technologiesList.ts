23a8fd73353ae9d9b1bab58f46de894feaaf2498
Angelbyte <roberto.angel96@live.com>
2026-03-20 13:31:50 -0600

    refactor(Technologies): renderizar tecnologías dinámicamente desde data mockeada
    - Se extrajo la lista de tecnologías a un archivo separado (technologiesList.ts)
    - Se reemplazaron las etiquetas `<li>` hardcodeadas mapeando el componente `TechItem`
    - Mejora la mantenibilidad y reduce código duplicado en Technologies.astro

---
+++ src/components/Technologies/mocks/technologiesList.ts
@@ -0,0 +1,90 @@
+export const technologiesList = {
+	frontEnd: [
+		{
+			name: 'HTML',
+			icon: 'html'
+		},
+		{
+			name: 'CSS',
+			icon: 'css'
+		},
+		{
+			name: 'JavaScript',
+			icon: 'js'
+		},
+		{
+			name: 'TypeScript',
+			icon: 'typescript'
+		},
+		{
+			name: 'Tailwind',
+			icon: 'tailwind'
+		}
+	],
+	backEnd: [
+		{
+			name: 'MySQL',
+			icon: 'mysql'
+		}
+	],
+	learning: [
+		{
+			name: 'React',
+			icon: 'react'
+		},
+		{
+			name: 'C#',
+			icon: 'csharp'
+		},
+		{
+			name: 'Astro',
+			icon: 'astro'
+		},
+		{
+			name: 'NextJS',
+			icon: 'nextjs'
+		},
+		{
+			name: 'NodeJS',
+			icon: 'nodejs'
+		},
+		{
+			name: 'ExpressJS',
+			icon: 'express'
+		},
+		{
+			name: 'Figma',
+			icon: 'figma'
+		},
+		{
+			name: 'Python',
+			icon: 'python'
+		}
+	],
+	tools: [
+		{
+			name: 'Git',
+			icon: 'git'
+		},
+		{
+			name: 'GitHub',
+			icon: 'github'
+		},
+		{
+			name: 'VS',
+			icon: 'Vs'
+		},
+		{
+			name: 'VSC',
+			icon: 'vsc'
+		},
+		{
+			name: 'Npm',
+			icon: 'npm'
+		},
+		{
+			name: 'Pnpm',
+			icon: 'pnpm'
+		}
+	]
+}
